7cecc5a4b618b4b1dcaa33047b84077198b852d5
Francis Tseng <f+accounts@frnsys.com>
2019-10-11 17:48:03 -0400

    init

---
+++ get_deps.py
@@ -0,0 +1,42 @@
+import json
+import time
+import lxml.html
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+driver = webdriver.Firefox()
+
+def get_deps(repo):
+    url = 'https://github.com{}/network/dependencies'.format(repo)
+    driver.get(url)
+    time.sleep(2)
+
+    try:
+        while True:
+            driver.find_element_by_css_selector('#dependencies .ajax-pagination-btn').click()
+            time.sleep(5)
+    except NoSuchElementException:
+        pass
+
+    html = lxml.html.fromstring(driver.page_source)
+    deps = [a.attrib['href'] for a in html.cssselect('#dependencies .js-dependency [data-octo-click=dep_graph_package]')]
+    return deps
+
+with open('repos.json', 'r') as f:
+    repos = json.load(f)
+
+for i, repo in tqdm(enumerate(repos)):
+    if 'dependencies' in repo: continue
+    url = repo['url']
+    deps = get_deps(url)
+    repo['dependencies'] = deps
+    if i % 10 == 0:
+        with open('repos.json', 'w') as f:
+            json.dump(repos, f)
+
+with open('repos.json', 'w') as f:
+    json.dump(repos, f)
+
+driver.close()
---
+++ get_repos.py
@@ -0,0 +1,93 @@
+import json
+import requests
+import lxml.html
+from tqdm import tqdm
+
+
+def get_repo(url):
+    resp = requests.get('https://github.com{}'.format(url))
+    html = lxml.html.fromstring(resp.content)
+    stars = html.cssselect('.social-count')[1].text
+
+    try:
+        desc = html.cssselect('[itemprop=about]')[0].text.strip()
+    except IndexError:
+        desc = None
+
+    tags = []
+    for t in html.cssselect('.topic-tag'):
+        tag = t.text.strip()
+        tags.append(tag)
+
+    return {
+        'url': url,
+        'tags': tags,
+        'desc': desc,
+        'stars': int(stars)
+    }
+
+
+repos = []
+new_topics = set()
+topics = open('topics.txt').read().splitlines()
+
+seen = set()
+bar = tqdm(topics)
+for topic in bar:
+    bar.set_description(topic)
+    resp = requests.get('https://github.com/topics/{}'.format(topic))
+    html = lxml.html.fromstring(resp.content)
+
+    if b'ajax-pagination-btn' in resp.content:
+        print('LOAD MORE FOR', topic)
+
+    for repo in html.cssselect('article'):
+        url = repo.cssselect('h1 a:last-child')[0].attrib['href']
+
+        # Probably an issue, not a repo
+        if url.count('/') > 2: continue
+
+        # Already encountered
+        if url in seen: continue
+
+        repo = get_repo(url)
+        for t in repo['tags']:
+            if t not in topics:
+                new_topics.add(t)
+        repos.append(repo)
+        seen.add(url)
+
+terms = open('terms.txt').read().splitlines()
+bar = tqdm(terms)
+for term in bar:
+    bar.set_description(term)
+    page = 1
+    while True:
+        resp = requests.get('https://github.com/search', params={'q': term, 'p': page})
+
+        html = lxml.html.fromstring(resp.content)
+        results = html.cssselect('.repo-list-item')
+        if not results:
+            break
+
+        for repo in results:
+            url = repo.cssselect('a')[0].attrib['href']
+
+            # Already encountered
+            if url in seen: continue
+
+            repo = get_repo(url)
+            for t in repo['tags']:
+                if t not in topics:
+                    new_topics.add(t)
+            repos.append(repo)
+            seen.add(url)
+        page += 1
+
+print(len(repos), 'repos')
+
+with open('repos.json', 'w') as f:
+    json.dump(repos, f)
+
+with open('new_topics.txt', 'w') as f:
+    f.write('\n'.join(list(new_topics)))
